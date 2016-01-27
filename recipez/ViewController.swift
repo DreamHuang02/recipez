e39ba957aca615f5303f6a35f558b02e85fd4e63
Dream Huang <dreamhuang02@hotmail.com>
2016-01-27 17:40:18 +0800

    Initial Commit

---
+++ recipez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  recipez
+//
+//  Created by Dream Huang on 27/01/2016.
+//  Copyright © 2016 Dream Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
